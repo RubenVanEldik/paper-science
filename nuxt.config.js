969fdbd86081baf7163eb4b1f195f68166f24afd
Ruben <ruben@hackerdaily.io>
2020-11-21 20:36:35 +0100

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,29 @@
+export default {
+  ssr: false,
+  target: 'static',
+  head: {
+    title: 'paper-science',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  css: [
+  ],
+  plugins: [
+  ],
+  components: true,
+  buildModules: [
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/tailwindcss'
+  ],
+  modules: [
+    '@nuxtjs/pwa'
+  ],
+  build: {
+  }
+}
