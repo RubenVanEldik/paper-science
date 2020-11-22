9842222e4abdd8bb9ed9e601329873a21786adbc
Ruben <rubenvaneldik>
2020-11-23 00:20:14 +0100

    refactor: Move the icon to its own component

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,7 @@
 <template>
   <div class="h-screen flex flex-col justify-center items-center">
-    <div class="text-green-600 mb-24">
-      <svg
-        class="h-16 fill-current"
-        aria-hidden="true"
-        focusable="false"
-        data-icon="scroll"
-        role="img"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 640 512"
-      >
-        <path
-          fill="currentColor"
-          d="M48 0C21.53 0 0 21.53 0 48v64c0 8.84 7.16 16 16 16h80V48C96 21.53 74.47 0 48 0zm208 412.57V352h288V96c0-52.94-43.06-96-96-96H111.59C121.74 13.41 128 29.92 128 48v368c0 38.87 34.65 69.65 74.75 63.12C234.22 474 256 444.46 256 412.57zM288 384v32c0 52.93-43.06 96-96 96h336c61.86 0 112-50.14 112-112 0-8.84-7.16-16-16-16H288z"
-        />
-      </svg>
+    <div class="text-green-600 mb-28">
+      <icon class="h-16" />
     </div>
     <form
       class="flex w-full max-w-md md:max-w-lg px-3"
