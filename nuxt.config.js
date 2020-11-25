ec98c7d8556457abcda0da3eb55805f115d0a538
Ruben <rubenvaneldik>
2020-11-25 15:25:21 +0100

    chore: Remove all meta tags from nuxt.config, since the PWA module already sets those

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,11 +8,6 @@ export default {
   },
   head: {
     title: 'Paper',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
-    ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
