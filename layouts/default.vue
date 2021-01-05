d421c4942c8c20d9a89c45de5e86f9db257810f0
Ruben <contact@abriefhistoryofyesterday.com>
2021-01-05 17:32:59 +0100

    fix: Don't use vue-100vh anymore

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,13 @@
 <template>
-  <vue100vh>
+  <div class="h-screen">
     <main class="h-full w-screen m-auto">
       <nuxt />
     </main>
-  </vue100vh>
+  </div>
 </template>
 
 <script>
-import Vue100vh from 'vue-100vh'
-
 export default {
-  components: {
-    Vue100vh
-  },
   head: {
     titleTemplate (titleChunk) {
       return (titleChunk && titleChunk !== 'Paper') ? `${titleChunk} | Paper` : 'Paper'
